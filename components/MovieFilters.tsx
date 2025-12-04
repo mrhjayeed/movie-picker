976a223c961bb1e500dadae14be534e252f60081
M. R. Haque Jayeed <mrhjayeed@gmail.com>
2025-12-04 22:10:03 +0600

    fix: dropdown opens to right on desktop to prevent cutoff

--- components/MovieFilters.tsx
+++ components/MovieFilters.tsx
@@ -70,7 +70,7 @@ export default function MovieFilters({ onFilterChange, isLoading }: MovieFilters
           />
           
           {/* Dropdown panel - bottom sheet on mobile, dropdown on desktop */}
-          <div className="fixed inset-x-0 bottom-0 rounded-t-xl sm:rounded-xl sm:absolute sm:bottom-auto sm:left-0 sm:right-auto sm:top-full sm:mt-2 sm:w-72 p-4 pb-8 sm:pb-4 bg-neutral-900 border-t sm:border border-neutral-800 shadow-xl z-50">
+          <div className="fixed inset-x-0 bottom-0 rounded-t-xl sm:rounded-lg sm:absolute sm:bottom-auto sm:right-0 sm:left-auto sm:top-full sm:mt-2 sm:w-72 p-4 pb-8 sm:pb-4 bg-neutral-900 border-t sm:border border-neutral-800 shadow-xl z-50">
             {/* Mobile drag handle */}
             <div className="flex justify-center mb-3 sm:hidden">
               <div className="w-10 h-1 bg-neutral-700 rounded-full" />
